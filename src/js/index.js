efa2b0cbf7e91a3ecc46019694b2bd40fb7c80a1
Hong Dah Ong <40519064+ezpher@users.noreply.github.com>
2020-02-22 20:08:13 +0800

    comment out test code for webpack and babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-// Global app controller
-import num from './test'; // ES6 import syntax
+/* testing babel and webpack */
+// import num from './test'; // ES6 import syntax
 
-console.log(`I imported ${num} from another module!`);
+// console.log(`I imported ${num} from another module!`);
+
+// Global app controller
